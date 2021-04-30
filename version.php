b1955d3e3a4da59ccefb0a13338c89199c8d9a4b
Stefan Scholz <stefanscholz@mac-mini-von-stefan.local>
2021-04-30 18:28:22 +0000

    fix supported versions

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ $plugin->component = 'availability_sectioncompleted';
 $plugin->version = 2021043000;
 $plugin->release = 'Version 1.0';
 $plugin->requires = 2020061500;
-$plugin->supported = [39, 310, 311];
+$plugin->supported = [39, 39];
 $plugin->maturity = MATURITY_STABLE;
