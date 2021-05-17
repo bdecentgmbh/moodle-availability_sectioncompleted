31cb4c42d016f40282f9a2d5137b45ac273a0fd7
Stefan Scholz <stefanscholz@mac-mini-von-stefan.local>
2021-05-17 17:53:55 +0000

    bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'availability_sectioncompleted';
-$plugin->version = 2021043000;
+$plugin->version = 2021043001;
 $plugin->release = 'Version 1.0';
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
