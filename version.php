30a7e45fc178596d9b36c7349db91f44bd3acd4c
Stefan-Alexander Scholz <hello@bdecent.de>
2025-04-11 12:07:35 +0200

    Update version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'availability_sectioncompleted';
 $plugin->version = 2025032401;
-$plugin->release = 'Version 1.03';
+$plugin->release = 'Version 1.1';
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [39, 405];
